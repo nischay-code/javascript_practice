b325db8961fc293deaf7927bcea092a254a66551
nischay-code <nischay.patel.code@gmail.com>
2022-07-30 18:03:33 +0530

    Types Done

---
+++ Types/types.js
@@ -0,0 +1,16 @@
+console.log("It Works");
+const name = "hero"
+const sentence = 'He\'s so cool'; /*backslash because it leans back*/
+//const sentence = "He's so cool"; 
+
+const multilineString = `hey
+you
+man`;
+
+const hello="Hey My name is " + name + "."
+const yo=`Hey My name is ${name}.`
+
+const object={
+    first:"hey",
+    age:50
+}
