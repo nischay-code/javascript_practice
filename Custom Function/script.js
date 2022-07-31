454a8f191ccec9380363d22c1cca015b261d69ba
nischay-code <nischay.patel.code@gmail.com>
2022-07-31 18:05:34 +0530

    Custom Function

---
+++ Custom Function/script.js
@@ -0,0 +1,12 @@
+//function definition
+console.log("It Works");
+function hello() {
+  //this is the function body
+  const add = 12 + 23;
+  //console.log(add);
+  return add;
+}
+//function call or run
+
+const hey = hello();
+console.log(`The Addition is ${hey} `);
--- Types/types.js
+++ Types/types.js
@@ -14,3 +14,4 @@ const object={
     first:"hey",
     age:50
 }
+
